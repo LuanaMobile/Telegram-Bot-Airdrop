066fdc2ae789f6cce9da17302133d65aa9da4730
LuanaMobile <144249410+LuanaMobile@users.noreply.github.com>
2024-10-20 23:43:44 +0300

    Update bot.py

--- cyberfinance/bot.py
+++ cyberfinance/bot.py
@@ -5,26 +5,29 @@ sys.dont_write_bytecode = True
 from smart_airdrop_claimer import base
 from core.token import get_token
 from core.info import game_data
-from core.task import process_do_task, process_watch_ads
+from core.task import process_check_in, process_do_task, process_watch_ads
 from core.claim import process_claim
 from core.boost import process_buy_boost
 
 import time
 
-
 class CyberFinanace:
     def __init__(self):
-        # Dosya dizinini al
+        # Get file directory
         self.data_file = base.file_path(file_name="data.txt")
         self.config_file = base.file_path(file_name="config.json")
 
-        # Çizgi oluştur
+        # Initialize line
         self.line = base.create_line(length=50)
 
-        # Banner oluştur
+        # Initialize banner
         self.banner = self.create_banner()
 
-        # Konfigürasyonu al
+        # Get config
+        self.auto_check_in = base.get_config(
+            config_file=self.config_file, config_name="auto-check-in"
+        )
+
         self.auto_do_task = base.get_config(
             config_file=self.config_file, config_name="auto-do-task"
         )
@@ -42,18 +45,24 @@ class CyberFinanace:
         )
 
     def create_banner(self):
-        # Banner'ı oluştur
-        banner_lines = [
-            f"\033[91m{'DEV: LuanaMobile':^45}\033[0m",  # Kırmızı
-            f"\033[97m{'https://t.me/Luanamobile':^30}\033[0m",  # Beyaz
-            f"\033[93m{'Yazılım, düşüncelerin kelimelere döküldüğü bir dildir; doğru kelimeleri bulmak, en güzel eserleri yaratır.':^50}\033[0m"  # Sarı
+        red = "\033[91m"
+        white = "\033[97m"
+        yellow = "\033[93m"
+        reset = "\033[0m"
+
+        banner_text = [
+            f"{red}DEV: LuanaMobile{reset}",
+            f"{white}https://t.me/Luanamobile{reset}",
+            f"{yellow}Yazılım, düşüncelerin kelimelere döküldüğü bir dildir; doğru kelimeleri bulmak, en güzel eserleri yaratır.{reset}"
         ]
-        return "\n".join(banner_lines)
+
+        formatted_banner = "\n".join(["   " + line for line in banner_text])
+        return formatted_banner
 
     def main(self):
         while True:
             base.clear_terminal()
-            print(self.banner)
+            print("\n" + self.banner)  # Üstten bir satır boşluk bıraktık
             data = open(self.data_file, "r").read().splitlines()
             num_acc = len(data)
             base.log(self.line)
@@ -61,7 +70,7 @@ class CyberFinanace:
 
             for no, data in enumerate(data):
                 base.log(self.line)
-                base.log(f"{base.green}Hesap numarası: {base.white}{no + 1}/{num_acc}")
+                base.log(f"{base.green}Hesap numarası: {base.white}{no+1}/{num_acc}")
 
                 try:
                     token = get_token(data=data)
@@ -70,47 +79,54 @@ class CyberFinanace:
                         balance = game_data(token=token)
                         base.log(f"{base.green}Bakiye: {base.white}{balance:,}")
 
-                        # Görev yap
+                        # Check in
+                        if self.auto_check_in:
+                            base.log(f"{base.yellow}Otomatik Check-in: {base.green}AÇIK")
+                            process_check_in(token=token)
+                        else:
+                            base.log(f"{base.yellow}Otomatik Check-in: {base.red}KAPALI")
+
+                        # Do task
                         if self.auto_do_task:
-                            base.log(f"{base.yellow}Otomatik Görev Yap: {base.green}AÇIK")
+                            base.log(f"{base.yellow}Otomatik Görev: {base.green}AÇIK")
                             process_do_task(token=token)
                         else:
-                            base.log(f"{base.yellow}Otomatik Görev Yap: {base.red}KAPALI")
+                            base.log(f"{base.yellow}Otomatik Görev: {base.red}KAPALI")
 
-                        # Reklam izle
+                        # Watch ads
                         if self.auto_watch_ads:
-                            base.log(f"{base.yellow}Otomatik Reklam İzle: {base.green}AÇIK")
+                            base.log(f"{base.yellow}Otomatik Reklam İzleme: {base.green}AÇIK")
                             process_watch_ads(token=token)
                         else:
-                            base.log(f"{base.yellow}Otomatik Reklam İzle: {base.red}KAPALI")
+                            base.log(f"{base.yellow}Otomatik Reklam İzleme: {base.red}KAPALI")
 
-                        # İddia et
+                        # Claim
                         if self.auto_claim:
-                            base.log(f"{base.yellow}Otomatik İddia Et: {base.green}AÇIK")
+                            base.log(f"{base.yellow}Otomatik Talep: {base.green}AÇIK")
                             process_claim(token=token)
                         else:
-                            base.log(f"{base.yellow}Otomatik İddia Et: {base.red}KAPALI")
+                            base.log(f"{base.yellow}Otomatik Talep: {base.red}KAPALI")
 
-                        # Çekiç al
+                        # Buy Hammer
                         if self.auto_buy_hammer:
-                            base.log(f"{base.yellow}Otomatik Çekiç Al: {base.green}AÇIK")
+                            base.log(f"{base.yellow}Otomatik Çekiç Alımı: {base.green}AÇIK")
                             hammer_limit_price = 10000
                             process_buy_boost(
                                 token=token, limit_price=hammer_limit_price
                             )
                         else:
-                            base.log(f"{base.yellow}Otomatik Çekiç Al: {base.red}KAPALI")
+                            base.log(f"{base.yellow}Otomatik Çekiç Alımı: {base.red}KAPALI")
 
                         balance = game_data(token=token)
                         base.log(f"{base.green}Bakiye: {base.white}{balance:,}")
                     else:
-                        base.log(f"{base.red}Token bulunamadı! Lütfen yeni sorgu kimliği alın")
+                        base.log(f"{base.red}Token bulunamadı! Lütfen yeni bir sorgu id'si alınız.")
                 except Exception as e:
                     base.log(f"{base.red}Hata: {base.white}{e}")
 
             print()
             wait_time = 60 * 60
-            base.log(f"{base.yellow}Bekle {int(wait_time/60)} dakika!")
+            base.log(f"{base.yellow}Bekleyin {int(wait_time/60)} dakika!")
             time.sleep(wait_time)
 
 
